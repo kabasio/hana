6c33a792215a307ba07a3edad132dc72b7175d80
s000720 <s000720@pc-000720-2.local>
2018-05-27 16:23:35 +0900

    add jQuery animation

--- hana.js
+++ hana.js
@@ -1,5 +1,49 @@
 console.log("hi");
 
-$(function() {
-    $('h1').hide().fadeIn('1000');
-  });
+  $(window).on('load',function(){
+	
+	// fade-up
+    $(window).scroll(function (){
+        $('.fade-up').each(function(){
+            var POS = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+
+            if (scroll > POS - windowHeight){
+                $(this).css({
+                        'opacity':'1',
+                        'transform':'translateY(0)',
+                        '-webkit-transform':'translateY(0)',
+                        '-moz-transform':'translateY(0)',
+                        '-ms-transform':'translateY(0)'
+                });
+            } else {
+                $(this).css({
+                        'opacity':'0',
+                        'transform':'translateY(70px)',
+                        '-webkit-transform':'translateY(70px)',
+                        '-moz-transform':'translateY(70px)',
+                        '-ms-transform':'translateY(70px)'
+                });
+            }
+        });
+    });
+});
+
+$(window).on('load',function(){
+
+	// fade-in
+    $(window).scroll(function (){
+        $('.fade-in').each(function(){
+            var POS = $(this).offset().top;  //fade-inがついている要素の位置
+            var scroll = $(window).scrollTop();  //スクロール一
+            var windowHeight = $(window).height();  //ウィンドウの高さ
+
+            if (scroll > POS - windowHeight + windowHeight/5){
+                $(this).css("opacity","1" );
+            } else {
+                $(this).css("opacity","0" );
+            }
+        });
+    });
+});
